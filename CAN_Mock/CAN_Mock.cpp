5ddbb3fb27fb97d4be2e502810010170df1382a4
a64070ho <harry.obrien@student.manchester.ac.uk>
2021-03-20 11:09:58 +0000

    Update mock to adapt to new naming

--- CAN_Mock/CAN_Mock.cpp
+++ CAN_Mock/CAN_Mock.cpp
@@ -4,7 +4,7 @@ bool CAN_Mock::mayInitialise = true;
 bool CAN_Mock::message_available = true;
 bool CAN_Mock::may_send_message = true;
 
-CAN_Frame CAN_Mock::rx_frame = Echo_Request_Packet(32);
+CAN_Frame CAN_Mock::rx_frame = Echo_Request_Packet::serialise(32);
 CAN_Frame CAN_Mock::latest_tx_frame;
 
 void CAN_Mock::set_packet_rx(CAN_Frame frame)
