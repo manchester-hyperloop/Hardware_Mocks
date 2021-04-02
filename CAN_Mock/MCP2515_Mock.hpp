2de020d5e01d7cd711e5aa8ba94ae8e580d8eb9b
a64070ho <harry.obrien@student.manchester.ac.uk>
2021-04-02 21:32:03 +0100

    Add functionality to force set_bitrate and set_mode to fail on demand

--- CAN_Mock/MCP2515_Mock.hpp
+++ CAN_Mock/MCP2515_Mock.hpp
@@ -17,69 +17,71 @@
 
 struct can_frame
 {
-    uint32_t can_id; /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-    uint8_t can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
-    uint8_t data[CAN_MAX_DLEN] __attribute__((aligned(8)));
+	uint32_t can_id; /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+	uint8_t can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+	uint8_t data[CAN_MAX_DLEN] __attribute__((aligned(8)));
 };
 
 enum CAN_CLOCK
 {
-    MCP_20MHZ,
-    MCP_16MHZ,
-    MCP_8MHZ
+	MCP_20MHZ,
+	MCP_16MHZ,
+	MCP_8MHZ
 };
 
 enum CAN_SPEED
 {
-    CAN_5KBPS,
-    CAN_10KBPS,
-    CAN_20KBPS,
-    CAN_31K25BPS,
-    CAN_33KBPS,
-    CAN_40KBPS,
-    CAN_50KBPS,
-    CAN_80KBPS,
-    CAN_83K3BPS,
-    CAN_95KBPS,
-    CAN_100KBPS,
-    CAN_125KBPS,
-    CAN_200KBPS,
-    CAN_250KBPS,
-    CAN_500KBPS,
-    CAN_1000KBPS
+	CAN_5KBPS,
+	CAN_10KBPS,
+	CAN_20KBPS,
+	CAN_31K25BPS,
+	CAN_33KBPS,
+	CAN_40KBPS,
+	CAN_50KBPS,
+	CAN_80KBPS,
+	CAN_83K3BPS,
+	CAN_95KBPS,
+	CAN_100KBPS,
+	CAN_125KBPS,
+	CAN_200KBPS,
+	CAN_250KBPS,
+	CAN_500KBPS,
+	CAN_1000KBPS
 };
 
 struct MCP2515
 {
-    bool initialised;
-    uint8_t CS_Pin;
+	bool initialised;
+	uint8_t CS_Pin;
 
-    static bool mayInitialise;
-    static bool message_available;
-    static bool may_send_message;
+	static bool mayInitialise;
+	static bool message_available;
+	static bool may_send_message;
+	static bool may_set_mode;
+	static bool may_set_bitrate;
 
-    static can_frame rx_frame;
-    static can_frame latest_tx_frame;
+	static can_frame rx_frame;
+	static can_frame latest_tx_frame;
 
-    enum ERROR
-    {
-        ERROR_OK = 0,
-        ERROR_FAIL = 1,
-        ERROR_ALLTXBUSY = 2,
-        ERROR_FAILINIT = 3,
-        ERROR_FAILTX = 4,
-        ERROR_NOMSG = 5
-    };
+	enum ERROR
+	{
+		ERROR_OK = 0,
+		ERROR_FAIL = 1,
+		ERROR_ALLTXBUSY = 2,
+		ERROR_FAILINIT = 3,
+		ERROR_FAILTX = 4,
+		ERROR_NOMSG = 5
+	};
 
-    MCP2515(uint8_t _CS_Pin);
-    ERROR reset();
-    ERROR setNormalMode();
-    ERROR setBitrate(const CAN_SPEED canSpeed, CAN_CLOCK canClock);
+	MCP2515(uint8_t _CS_Pin);
+	ERROR reset();
+	ERROR setNormalMode();
+	ERROR setBitrate(const CAN_SPEED canSpeed, CAN_CLOCK canClock);
 
-    ERROR readMessage(can_frame *dst_frame);
-    ERROR sendMessage(can_frame *frame);
+	ERROR readMessage(can_frame *dst_frame);
+	ERROR sendMessage(can_frame *frame);
 
-    static void set_packet_rx(can_frame frame);
+	static void set_packet_rx(can_frame frame);
 };
 
 #include "../../lib/CAN_Interface/include/Packets.hpp"
